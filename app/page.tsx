65a0d3c8c7e856eb6fee64b8054fd1647ed5b073
Domenik Hofer <domenik.hofer@twofold.swiss>
2024-05-28 14:12:57 +0200

    Added decorations + actions to Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,18 @@
 
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 import getCategories from './libs/getCategories'
 import CategoryActions from './components/CategoryActions'
+import plantTop from '/public/images/plantTop.png'
+import plantSide from '/public/images/plantSide.png'
+import plantBottom from '/public/images/plantBottom.png'
 
 export default function Home() {
 
-// TODO: move into components?
-
   const [categories, setCategories] = useState<Category[]>([])
-  const [editMode, setEditMode] = useState(true)
+  const [editMode, setEditMode] = useState(false)
+  const [showEditBtn, setShowEditBtn] = useState(false)
 
   useEffect(() => {
     getCategories().then((categories: any) => {
@@ -18,18 +21,24 @@ export default function Home() {
     })
   }, []);
 
-  const categoriesList = categories.map((category) =>
+  const categoriesList = categories.map((category: Category) =>
     <>
-      <div key={category.id} className="category categoriesTitle">
+      <div key={category.id} id={`cat${category.id}`} className="category categoriesTitle">
         <div className="content">
           <h2>{category.name}</h2>
         </div>
         <CategoryActions category={category} className={`${editMode ? 'visible' : ''}`} />
       </div>
       <div className="subcategoriesWrapper">
+        {category.id == 9 &&
+          <div className={`plantSide ${!editMode ? 'visible' : ''}`}>
+            <Image src={plantSide} alt="" />
+          </div>
+        }
+
         <div className="subcategories">
-          {category.subcategories.map((subcategory) =>
-            <div key={subcategory.id} className="category">
+          {category.subcategories.map((subcategory: Category) =>
+            <div key={subcategory.id} id={`cat${subcategory.id}`} className="category">
               <div className="content">
                 <Link href={`/category/${subcategory.id}`} className="link">
                   <span className="emoji">
@@ -44,6 +53,7 @@ export default function Home() {
                   </span>
                 </Link>
               </div>
+              <CategoryActions category={subcategory} className={`${editMode ? 'visible' : ''}`} />
             </div>
           )}
         </div>
@@ -52,8 +62,29 @@ export default function Home() {
   )
 
   return (
-    <div className={`categoriesWrapper ${editMode ? 'reorder' : ''}`}>
-      {categoriesList}
-    </div>
+    <>
+      <div className={`categoriesWrapper ${editMode ? 'reorder' : ''}`}>
+        {categoriesList}
+        <div className={`plantTop ${!editMode ? 'visible' : ''}`}>
+          <Image src={plantTop} alt="" />
+        </div>
+        <div className={`plantBottomWrapper ${showEditBtn ? 'clicked' : ''} ${editMode ? '' : 'visible'}`} onClick={() => setShowEditBtn(true)}>
+          <div className="plantBottom">
+            <Image src={plantBottom} alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="pageActions">
+        {editMode ?
+          <>
+            <Link className="button" href="/category/create">➕</Link>
+            <a className="button" onClick={() => { setEditMode(false); setShowEditBtn(false) }}>✖️</a>
+          </>
+          :
+          <>
+            <a className="button" onClick={() => setEditMode(true)}>✏️</a>
+          </>}
+      </div>
+    </>
   );
 }
